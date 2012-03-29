5ad74ce66a7a675fdad652720540a57191053d75
Jakhongir Alimov(ajahongir) <ajahongir@gmail.com>
2012-03-30 01:39:54 +0500

    default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Ievent::Application.configure do
   #for herocu
   config.assets.initialize_on_precompile = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://stark-sunrise-7032.herokuapp.com' }
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.smtp_settings = {
