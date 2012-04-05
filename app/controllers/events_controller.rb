30d144580fe1ac9482458218254d2ae4707fdcb5
ajahongir <ajahongir@ajahongir-pc.(none)>
2012-04-05 21:59:55 +0500

    test tab space settings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,37 @@
 class EventsController < ApplicationController
 
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
   before_filter :check_date, :only => [:create, :update]
 
-	def index
-		if request.xhr?
-			result = current_user.events.map {|event|
-				record = {:id => event.id, :title => event.title.to_s, :allDay => event.allDay, :start => event.from, :end => event.to, :repeat => event.repeat || Event.notify_types.first, :className => (event.repeat || Event.notify_types.first).downcase}
-			}
-			render :json => result.to_json
-		end
-	end
+  def index
+    if request.xhr?
+      result = current_user.events.map {|event|
+        record = {:id => event.id, :title => event.title.to_s, :allDay => event.allDay, :start => event.from, :end => event.to, :repeat => event.repeat || Event.notify_types.first, :className => (event.repeat || Event.notify_types.first).downcase}
+      }
+      render :json => result.to_json
+    end
+  end
 
-	def list
+  def list
     @events = current_user.events
   end
 
-	def create
-	  event = current_user.events.create(:title => params[:title], :repeat => params[:repeat], :from => @from, :to => @to, :allDay => @allDay)
-		render :json => {:id => event.id, :success => true}
-	end
+  def create
+    event = current_user.events.create(:title => params[:title], :repeat => params[:repeat], :from => @from, :to => @to, :allDay => @allDay)
+    render :json => {:id => event.id, :success => true}
+  end
 
-	def update
-		event = Event.find(params[:id])
-		event.update_attributes({:title => params[:title], :repeat => params[:repeat], :from => @from, :to => @to, :allDay => @allDay})
-		render :nothing => true
-	end
+  def update
+    event = Event.find(params[:id])
+    event.update_attributes({:title => params[:title], :repeat => params[:repeat], :from => @from, :to => @to, :allDay => @allDay})
+    render :nothing => true
+  end
 
-	def destroy
-  	@event = current_user.events.find(params[:id])
-		render :nothing => true and return unless @event
-		@event.destroy
-	end
+  def destroy
+    @event = current_user.events.find(params[:id])
+    render :nothing => true and return unless @event
+    @event.destroy
+  end
 
   private
 
